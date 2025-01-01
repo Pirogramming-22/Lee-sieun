45248477054a565df89c21e93fcaad122e603408
iseun15 <ave0315@naver.com>
2025-01-01 22:11:40 +0900

    1-6 JavaScript-display items

--- Javascript 쇼핑몰 미니게임 과제/main.js
+++ Javascript 쇼핑몰 미니게임 과제/main.js
@@ -1,15 +1,29 @@
-//json에서 item 받아오기
+//json에서 item 받아오기 -> 받아오는 걸 성공하면 json 타입으로 변경
 function loadItems(){
-    return fetch('data/data.json').then(response => response.json())
-    .then(json => console.log(json));
+    return fetch('data/data.json')
+    .then(response => response.json())
+    .then(json => json.items);
 }
 
+//items를 list로 업데이트트
+function displayItems(items){
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
 
-
+//주어진 item에서 HTML list를 생성함함
+function createHTMLString(item){
+    return `
+    <li class="item">
+            <img src="${item.image}" alt="${item.type}" class="item_thumbnail">
+            <span class="item__description">${item.gender}, ${item.size}</span>
+        </li>
+    `;
+}
 //main
 loadItems()
     .then(items => {
-        // displayItems(items);
+        displayItems(items);
         // setEventListners(items)
     })
 .catch(console.log)
