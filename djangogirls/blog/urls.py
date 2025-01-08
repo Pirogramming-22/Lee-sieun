b7785327d0ca10e1451987c21702cb785b67ea56
iseun15 <ave0315@naver.com>
2025-01-09 01:26:34 +0900

    HTML 시작하기

--- djangogirls/blog/urls.py
+++ djangogirls/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
 ]
 
 #post_list라는 view 가 루트 URL에 할당 -> 누군가 주소로 들어왔을 떄 viewx.post_list를 보여주라 함
