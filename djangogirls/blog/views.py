374f689c749c45314e710f160bd947f2daaffd02
iseun15 <ave0315@naver.com>
2025-01-09 01:53:25 +0900

    템플릿 동적 데이터

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Post  #동일한 디렉토리 내에 있어서 .models로 호출
+from django.utils import timezone
 
 def post_list(request): #요청을 넘겨받아 render메서드 호출
-    return render(request, 'blog/post_list.html',{}) #render 메서드 호출하여 받은 blog/post)list.html 템플릿 보여줌
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html',{'posts':posts}) #render 메서드 호출하여 받은 blog/post)list.html 템플릿 보여줌
 
