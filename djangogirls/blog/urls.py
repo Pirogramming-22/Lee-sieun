018c6d70070e016e140229a7f8c4fd6045c72cb6
iseun15 <ave0315@naver.com>
2025-01-09 02:55:22 +0900

    애플리케이션 확장하기

--- djangogirls/blog/urls.py
+++ djangogirls/blog/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
 
 #post_list라는 view 가 루트 URL에 할당 -> 누군가 주소로 들어왔을 떄 viewx.post_list를 보여주라 함
 #url 마다 고유의 name 짓는 것 중요
+
+#post/<int:pk>/ : url 패턴 => post 문자 포함 & pk를 정수로 생각하고 뷰로 전송 & 다음에 / 한번 더 와야함
--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post  #동일한 디렉토리 내에 있어서 .models로 호출
 from django.utils import timezone
 
@@ -6,3 +6,6 @@ def post_list(request): #요청을 넘겨받아 render메서드 호출
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')    #posts에 작성된 글의 목록이 들어감
     return render(request, 'blog/post_list.html',{'posts':posts}) #render 메서드 호출하여 받은 blog/post)list.html 템플릿 보여줌
 
+def post_detail(request,pk):
+    post=get_object_or_404(Post, pk=pk)   #pk=pk 만족하는 객체 존재하면 반환, 존재하지 않으면 404에러페이지 반환
+    return render(request, 'blog/post_detail.html', {'post': post})
