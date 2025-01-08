3244585d037b18a35cb4f7cfd4bfc83bd0fd3e2c
iseun15 <ave0315@naver.com>
2025-01-09 01:08:42 +0900

    Django urls

--- djangogirls/mysite/urls.py
+++ djangogirls/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include #*blog.urls 가져오려면 include 함수가 필요
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('',include('blog.urls')),
+    #*http://127.0.0.1:8000/ 로 들어오는 모든 접속 요청을 blog.urls로 전송해서 추가 명령을 찾게됨
 ]
